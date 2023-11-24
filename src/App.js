2afe5bcc90ee28bcae3bdaf9a2d7eb9092b7440d
rhonvyramos <rhonvyramos@gmail.com>
2023-11-24 12:10:33 -0500

    changed base app template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+// importing client facing page
+import Client_Facing from './client/pages/client_facing';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Client_Facing />
   );
 }
 
---
+++ src/client/pages/client_facing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Client_Facing() {
+    return(
+        <div id="client_facing">
+
+        </div>
+    )
+};
