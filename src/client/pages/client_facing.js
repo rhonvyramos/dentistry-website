8ce2c61d25762e52d17f6de64fa30480f9ef7af2
rhonvyramos <rhonvyramos@gmail.com>
2023-11-26 10:15:02 -0500

    content and footer skeleton implemented

--- src/client/pages/client_facing.js
+++ src/client/pages/client_facing.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Header from "../components/header.js";
+import Header from "../components/header/index.js";
+import Content from "../components/content/index.js";
+import Footer from "../components/footer/index.js";
 
 export default function Client_Facing() {
     return(
@@ -7,8 +9,12 @@ export default function Client_Facing() {
             <div id="header_container">
                 <Header />
             </div>
-            <div id="content_container"></div>
-            <div id="footer_container"></div>
+            <div id="content_container">
+                <Content />
+            </div>
+            <div id="footer_container">
+                <Footer />
+            </div>
         </section>
     )
 };
