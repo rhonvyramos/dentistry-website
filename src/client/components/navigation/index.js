1b21afaa254d89ec88e5cae65eacfe4f602caa69
rhonvyramos <rhonvyramos@gmail.com>
2023-11-27 11:11:40 -0500

    structured navigation tabs

--- src/client/components/navigation/index.js
+++ src/client/components/navigation/index.js
@@ -5,7 +5,18 @@ export default function Navigation() {
     return(
         <section id="navigation_elements">
             <div id="navigation_bar">
-                NAVIGATION
+                <div id="tab_about" className="navigation_tab">
+                    <p>About</p>
+                </div>
+                <div id="tab_location" className="navigation_tab">
+                    <p>Location</p>
+                </div>
+                <div id="tab_services" className="navigation_tab">
+                    <p>Services</p>
+                </div>
+                <div id="tab_contact" className="navigation_tab">
+                    <p>Contacts</p>
+                </div>
             </div>
         </section>
     )
